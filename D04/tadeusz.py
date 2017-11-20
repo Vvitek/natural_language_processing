b4f3eaf3a86af561cf4e77125e93598f5e77f491
Vvitek <wifacz@gmail.com>
2017-11-20 09:45:52 +0100

    Tadeusz done

--- D04/tadeusz.py
+++ D04/tadeusz.py
@@ -1,8 +1,9 @@
 from nltk.tokenize import word_tokenize
 from nltk.probability import FreqDist
+from re import sub
 
 fdist = FreqDist()
-for word in word_tokenize(open('pantadeusz.txt', 'r').read().strip(',.')):
+for word in word_tokenize(sub('[^\w\s]','', open('pantadeusz.txt', 'r').read())):
     fdist[word.lower()] += 1
 
 print(fdist.most_common(50))
